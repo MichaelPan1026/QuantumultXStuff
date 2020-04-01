4567633e00943747cc0141ff9f598c6b502d410a
Michael Pan <michaelpan.1026@gmail.com>
2020-04-01 19:08:05 +0800

    update

---
+++ check/xiaoheihe/qmkg.js
@@ -0,0 +1,70 @@
+const cookieName = '小黑盒'
+const signurlKey = 'senku_signurl_xhh'
+const signheaderKey = 'senku_signheader_xhh'
+const signbodyKey = 'senku_signbody_xhh'
+const senku = init()
+const signurlVal = senku.getdata(signurlKey)
+const signheaderVal = senku.getdata(signheaderKey)
+//const signBodyVal = senku.getdata(signbodyKey)
+
+sign()
+
+function sign() {
+  const url = { url: signurlVal, headers: JSON.parse(signheaderVal) /*, body: signBodyVal */ }
+  senku.get(url, (error, response, data) => {
+    const result = JSON.parse(data)
+    const coinNum = result.result.level_info.coin
+    const level = result.result.level_info.level
+    const exp = result.result.level_info.exp
+    const maxExp = result.result.level_info.max_exp
+    const msg = result.msg
+    let detail = ``
+    detail = `你现在等级为${level}（${exp}\/${maxExp}）级`
+    senku.msg(cookieName, msg, detail)
+    senku.done()
+  })
+}
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
