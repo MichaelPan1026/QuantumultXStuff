fdaf1172e46765f72dd879cb1b7cc3865ce2eee9
Michael Pan <michaelpan.1026@gmail.com>
2021-09-24 15:22:52 +0800

    Update wc.js

--- config/wc.js
+++ config/wc.js
@@ -4,8 +4,8 @@ obj["subscriber"]["subscriptions"]= {
         "is_sandbox" : false,
         "ownership_type" : "PURCHASED",
         "billing_issues_detected_at" : null,
-        "period_type" : "active",
-        "expires_date" : "2099-12-01T03:51:32Z",
+        "period_type" : "trial",
+        "expires_date" : "2021-09-27T06:33:41Z",
         "grace_period_expires_date" : null,
         "unsubscribe_detected_at" : null,
         "original_purchase_date" : "2021-09-24T06:33:41Z",
@@ -18,7 +18,7 @@ obj["subscriber"]["entitlements"]= {
         "grace_period_expires_date" : null,
         "purchase_date" : "2021-09-24T06:33:41Z",
         "product_identifier" : "wc_pro_1m",
-        "expires_date" : "2099-12-01T03:51:32Z"
+        "expires_date" : "2021-09-27T06:33:41Z"
     }
 };
 $done({body: JSON.stringify(obj)});
